67a8d7ca7bda58d0c5b0707f5bf7b23b4a9ec763
djurslinn <djurslinnjameskm@gmail.com>
2026-06-09 03:02:11 -0400

    models.py lint error fixed

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,18 +1,44 @@
+# Uncomment the following imports before adding the Model code
+
 from django.db import models
-from django.utils.timezone import now  # removed unused warning by keeping optional usage
+from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+# Create your models here.
+
+# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# - Name
+# - Description
+# - Any other fields you would like to include in car make model
+# - __str__ method to print a car make object
+
+
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# - Many-To-One relationship to Car Make model (One Car Make has many
+# Car Models, using ForeignKey field)
+# - Name
+# - Type (CharField with a choices argument to provide limited choices
+# such as Sedan, SUV, WAGON, etc.)
+# - Year (IntegerField) with min value 2015 and max value 2023
+# - Any other fields you would like to include in car model
+# - __str__ method to print a car make object
 
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField()
+    # Other fields as needed
 
     def __str__(self):
-        return self.name
+        return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )  # Many-to-One relationship
 
     name = models.CharField(max_length=100)
 
@@ -20,6 +46,7 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
+        # Add more choices as required
     ]
 
     type = models.CharField(
@@ -36,5 +63,7 @@ class CarModel(models.Model):
         ]
     )
 
+    # Other fields as needed
+
     def __str__(self):
-        return self.name
+        return self.name  # Return the name as the string representation
